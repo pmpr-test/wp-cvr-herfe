f2d73c442954150d0b690b5141b5041b4b222cc2
pmpr.dev <pmpr.dev@gmail.com>
2024-10-31 10:10:31 +0000

    Obfuscated

--- src/Pmpr/Cover/Herfe/Page/Common.php
+++ src/Pmpr/Cover/Herfe/Page/Common.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             6723571d95406             |
+    |             672357617e71a             |
     |_______________________________________|
 */
  namespace Pmpr\Cover\Herfe\Page; use Pmpr\Common\Foundation\Page\Page; abstract class Common extends Page { }
